eaea62a3962e517913b94fbead6c050ab536955c
Zoey Chesny <z.chesny@gmail.com>
2020-04-09 18:27:55 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,6 +1,6 @@
 # Write your code here.
 def batch_badge_creator(names)
-  names.map!{|name| "Hello, my name is #{name}."}
+  names.map{|name| "Hello, my name is #{name}."}
 end
 
 def assign_rooms(speakers)
